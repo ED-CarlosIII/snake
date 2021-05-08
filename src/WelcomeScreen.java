14c101cd673d90d209333870a5d0ad9befdb5d96
Juanjo <Juanjo@DESKTOP-P0D4AIG>
2021-05-08 03:04:16 +0200

    refactorización realizada

--- src/WelcomeScreen.java
+++ src/WelcomeScreen.java
@@ -1,5 +1,4 @@
-import java.awt.CardLayout;
-import java.awt.FlowLayout;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -9,53 +8,56 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
+public class WelcomeScreen extends JPanel {
+    
+    private static final JButton J_BUTTON_QUIT = new JButton("Quit");
+    private static final JButton J_BUTTON_GO = new JButton("New Game");
+    
+    JLabel title;
+    JButton go;
+    JButton quit;
 
-public class WelcomeScreen extends JPanel{
-	JLabel title;
-	JButton go;
-	JButton quit;
-	
-	MainWindow mw;
-	
-	public void setTitle(String t){
-		title.setText(t);
-	}
-	
-	public void quitButtonActionListener(){
-		if(JOptionPane.showConfirmDialog(this, "Are you sure?") == JOptionPane.OK_OPTION)
-	        System.exit(0);
-	}
-	
-	public void goButtonActionListener(){
-		mw.showCard("Two");
-	}
- 
-	public WelcomeScreen(MainWindow mw){
-		this.mw = mw;
-		
-		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-		title = new JLabel();
-		add(title);
-		
-		//add formatting here
-		
-		go = new JButton("New Game");
-		quit = new JButton("Quit");	
-		
-		go.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent event){
-				goButtonActionListener();
-			}
-		});
-		
-		quit.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent event){
-				 quitButtonActionListener();
-			}
-		});
-		
-		add(go);
-		add(quit);
-	}
+    MainWindow mw;
 
+    public void setTitle(String t) {
+        title.setText(t);
+    }
+
+    public void quitButtonActionListener() {
+        if (JOptionPane.showConfirmDialog(this, "Are you sure?") == JOptionPane.OK_OPTION) {
+            System.exit(0);
+        }
+    }
+
+    public void goButtonActionListener() {
+        mw.showCard("Two");
+    }
+
+    public WelcomeScreen(MainWindow mw) {
+        this.mw = mw;
+
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        title = new JLabel();
+        add(title);
+
+        //add formatting here
+        go = J_BUTTON_GO;
+        quit = J_BUTTON_QUIT;
+
+        go.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                goButtonActionListener();
+            }
+        });
+
+        quit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                quitButtonActionListener();
+            }
+        });
+
+        add(go);
+        add(quit);
+    }
+    
 }
