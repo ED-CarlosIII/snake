d644338c4ab1fa8b6de42c0113ac8f09354e7f13
joslopcar <83919320+joslopcar@users.noreply.github.com>
2021-05-09 20:50:45 +0200

    Add files via upload

---
+++ snake-refactorizacion_documentacion/src/PlayerSelectScreen.java
@@ -0,0 +1,123 @@
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import static javax.swing.BoxLayout.PAGE_AXIS;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+
+//needs massive aesthetic rewrites
+
+/**
+ *
+ * 
+ */
+public class PlayerSelectScreen extends JPanel {
+
+    JButton go;
+    JButton quit;
+    MainWindow mw;
+
+    JRadioButton opt1;
+    JRadioButton opt2;
+    JRadioButton opt3;
+
+    /**
+     *
+     */
+    public void quitButtonActionListener() {
+        mw.showCard("One");
+    }
+
+    /**
+     *
+     */
+    public void goButtonActionListener() {
+        playerOptions();
+        mw.s4.setUpPlayers();
+        mw.showCard("Three");
+    }
+
+    /**
+     *
+     */
+    public void playerOptions() {
+        int m = 5;
+        if (opt1.isSelected() == true) {
+            m = 1;
+        } else if (opt2.isSelected() == true) {
+            m = 2;
+        } else if (opt3.isSelected() == true) {
+            m = 3;
+        }
+        mw.s4.setMaxPlayers(m);
+    }
+
+    /**
+     *
+     * @param mw
+     */
+    public PlayerSelectScreen(MainWindow mw) {
+        this.mw = mw;
+
+        String player1 = "1 player (Default)";
+        String player2 = "2 players";
+        String player3 = "3 players";
+
+        setLayout(new BoxLayout(this, PAGE_AXIS));
+
+        JLabel mess = new JLabel("Select players: ");
+        add(mess);
+
+        JLabel uc = new JLabel("Default: 1 player");
+        add(uc);
+
+        //set up radio buttons
+        opt1 = new JRadioButton(player1);
+        opt1.setSelected(true);
+
+        opt2 = new JRadioButton(player2);
+
+        opt3 = new JRadioButton(player3);
+
+        ButtonGroup grp = new ButtonGroup();
+        grp.add(opt1);
+        grp.add(opt2);
+        grp.add(opt3);
+
+        add(opt1);
+        add(opt2);
+        add(opt3);
+
+        opt1.addActionListener((ActionEvent event) -> {
+            playerOptions();
+        });
+        opt2.addActionListener((ActionEvent event) -> {
+            playerOptions();
+        });
+
+        opt3.addActionListener((ActionEvent event) -> {
+            playerOptions();
+        });
+
+        go = new JButton("Customize Board");
+        quit = new JButton("Back");
+
+        go.addActionListener((ActionEvent event) -> {
+            goButtonActionListener();
+        });
+
+        quit.addActionListener((ActionEvent event) -> {
+            quitButtonActionListener();
+        });
+
+        add(go);
+        add(quit);
+
+    }
+
+}
---
+++ snake-refactorizacion_documentacion/src/Portal.java
@@ -0,0 +1,53 @@
+import java.awt.Point;
+import java.util.Random;
+
+/**
+ *
+ * 
+ */
+public class Portal {
+
+    private int nature; //-1 for snake , +1 for ladder
+    private int start;
+    private int end;
+
+    /**
+     *
+     * @param maxCells
+     */
+    public Portal(int maxCells) {   //creates random portals
+        Random luck = new Random();
+        start = luck.nextInt(maxCells);
+        end = luck.nextInt(maxCells);
+        if (start < end) {
+            nature = 1;
+        } else {
+            nature = -1;
+        }
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int returnNature() {
+        return nature;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int returnStart() {
+        return start;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int returnEnd() {
+        return end;
+    }
+
+}
